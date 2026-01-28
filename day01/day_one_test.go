dbb42ca8833c40a202c5494fd4b8d8ac8a3c5e8e
Grey Skipwith <grey.skipwith07@gmail.com>
2026-01-28 13:00:39 +0200

    Test: Renaming of variables

--- day01/day_one_test.go
+++ day01/day_one_test.go
@@ -3,25 +3,25 @@ package main
 import "testing"
 
 func TestKnobRotation(t *testing.T) {
-	expected1, expected2 := rotateKnob("small_entries.txt")
-	want1, want2 := 3, 6
-	if expected1 != want1 {
-		t.Fatalf("RotateKnob result is %d, want %d", expected1, want1)
+	result1, result2 := rotateKnob("small_entries.txt")
+	expected1, expected2 := 3, 6
+	if result1 != expected1 {
+		t.Fatalf("RotateKnob result is %d, want %d", result1, expected1)
 	}
 
-	if expected2 != want2 {
-		t.Fatalf("RotateKnob part 2 result is %d, want %d", expected2, want2)
+	if result2 != expected2 {
+		t.Fatalf("RotateKnob part 2 result is %d, want %d", result2, expected2)
 	}
 }
 
 func TestKnobRotation2(t *testing.T) {
-	expected1, expected2 := rotateKnob("other_entries.txt")
-	want1, want2 := 5, 22
-	if expected1 != want1 {
-		t.Fatalf("RotateKnob result is %d, want %d", expected1, want1)
+	result1, result2 := rotateKnob("other_entries.txt")
+	expected1, expected2 := 5, 22
+	if result1 != expected1 {
+		t.Fatalf("RotateKnob result is %d, want %d", result1, expected1)
 	}
 
-	if expected2 != want2 {
-		t.Fatalf("RotateKnob part 2 result is %d, want %d", expected2, want2)
+	if result2 != expected2 {
+		t.Fatalf("RotateKnob part 2 result is %d, want %d", result2, expected2)
 	}
 }
