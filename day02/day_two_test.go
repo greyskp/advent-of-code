aae4f2969005c132b8ae539ec35316a3128493ae
Grey Skipwith <grey.skipwith07@gmail.com>
2026-01-28 22:04:16 +0200

    test: update test Day02, Part 1

--- day02/day_two_test.go
+++ day02/day_two_test.go
@@ -4,8 +4,14 @@ import "testing"
 
 func TestIdentifyInvalidIds(t *testing.T) {
 	result := IdentifyInvalidIds("small_entries.txt")
+	result_large := IdentifyInvalidIds("entries.txt")
 	expected := 1227775554
+	expected_large := 40398804950
 	if result != expected {
 		t.Fatalf("IdentifyInvalidIds result is %d, want %d", result, expected)
+	}
+
+	if result_large != expected_large {
+		t.Fatalf("IdentifyInvalidIds result for large dataset is %d, want %d", result_large, expected_large)
 	}
 }
