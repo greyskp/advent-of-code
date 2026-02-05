b80fd4fb3514a53a603eeec31f442484e6f0c8a8
Grey Skipwith <grey.skipwith07@gmail.com>
2026-02-05 20:11:15 +0200

    feat: day05, part 2

--- day05/day_five.go
+++ day05/day_five.go
@@ -11,6 +11,22 @@ import (
 	"github.com/greyskp/advent-of-code/utils"
 )
 
+func parseRangeAndIngredients(filename string) ([][]int, []int) {
+	data := utils.ReadInputFile(filename)
+	inventory := strings.Split(data, "\n\n")
+
+	rangeList := parseRanges(strings.Split(inventory[0], "\n"))
+	ingredientsList := parseList(strings.Split(inventory[1], "\n"))
+
+	sort.Ints(ingredientsList)
+
+	sort.Slice(rangeList, func(i, j int) bool {
+		return rangeList[i][0] < rangeList[j][0]
+	})
+
+	return rangeList, ingredientsList
+}
+
 func parseList(list []string) []int {
 	parsedList := []int{}
 	for _, element := range list {
@@ -48,19 +64,10 @@ func parseRanges(ranges []string) [][]int {
 	return parsedRanges
 }
 
-func CountFreshIngredients(filename string) int {
+func CountFreshIngredientsFromList(filename string) int {
 	freshIngredients := 0
-	data := utils.ReadInputFile(filename)
-	inventory := strings.Split(data, "\n\n")
 
-	rangeList := parseRanges(strings.Split(inventory[0], "\n"))
-	ingredientsList := parseList(strings.Split(inventory[1], "\n"))
-
-	sort.Ints(ingredientsList)
-
-	sort.Slice(rangeList, func(i, j int) bool {
-		return rangeList[i][0] < rangeList[j][0]
-	})
+	rangeList, ingredientsList := parseRangeAndIngredients(filename)
 
 	for _, ingredient := range ingredientsList {
 		for _, range_ := range rangeList {
@@ -74,11 +81,29 @@ func CountFreshIngredients(filename string) int {
 	return freshIngredients
 }
 
+func CountAllFreshIngredients(filename string) int {
+	total := 0
+	rangeList, _ := parseRangeAndIngredients(filename)
+	latest := 0
+
+	for _, range_ := range rangeList {
+		start := max(range_[0], latest+1)
+
+		if start <= range_[1] {
+			total += range_[1] - start + 1
+			latest = range_[1]
+		}
+	}
+
+	return total
+}
+
 func main() {
 	path := "entries.txt"
 	if len(os.Args) > 1 {
 		path = os.Args[1]
 	}
 
-	fmt.Println(CountFreshIngredients(path))
+	fmt.Println(CountFreshIngredientsFromList(path))
+	fmt.Println(CountAllFreshIngredients(path))
 }
--- day05/day_five_test.go
+++ day05/day_five_test.go
@@ -5,16 +5,31 @@ import (
 )
 
 
-func TestCountFreshIngredients(t *testing.T) {
-	result := CountFreshIngredients("small_entries.txt")
-	resultLarge := CountFreshIngredients("entries.txt")
+func TestCountFreshIngredientsFromList(t *testing.T) {
+	result := CountFreshIngredientsFromList("small_entries.txt")
+	resultLarge := CountFreshIngredientsFromList("entries.txt")
 
 	expected := 3
+	if result != expected {
+		t.Fatalf("CountFreshIngredientsFromList result is %d, want %d", result, expected)
+	}
+
+	expectedLarge := 525
+	if resultLarge != expectedLarge {
+		t.Fatalf("CountFreshIngredientsFromList result is %d, want %d", resultLarge, expectedLarge)
+	}
+}
+
+func TestCountAllFreshIngredients(t *testing.T) {
+	result := CountAllFreshIngredients("small_entries.txt")
+	resultLarge := CountAllFreshIngredients("entries.txt")
+
+	expected := 14
 	if result != expected {
 		t.Fatalf("CountFreshIngredients result is %d, want %d", result, expected)
 	}
 
-	expectedLarge := 525
+	expectedLarge := 333892124923577
 	if resultLarge != expectedLarge {
 		t.Fatalf("CountFreshIngredients result is %d, want %d", resultLarge, expectedLarge)
 	}
